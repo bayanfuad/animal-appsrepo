e9d1159e62c2eb6f6a38cb9fd7be410c8dc4bc1f
bayanfuad <bayanjawawdeh9@gmail.com>
2022-07-25 23:52:31 +0300

    done

--- src/hornedBeasts.js
+++ src/hornedBeasts.js
@@ -1,11 +1,12 @@
 import React from "react"; 
+import Cards from "./cards";
 class Hornbeasts extends React.Component{
+
+    
     render(){
         return(
             <>
-            <h2>{this.props.title}</h2>
-            <img src={this.props.imgUrl} />
-            <p>{this.props.description}</p>
+           <Cards title ={this.props.title} image_url= {this.props.imgUrl} description ={this.props.description}/>
             </>    
         
         )
@@ -13,3 +14,26 @@ class Hornbeasts extends React.Component{
     }
 }
 export default Hornbeasts;
+
+
+
+
+
+
+
+    
+//     // onClick={this.handleClick}
+    
+//     handleClick=()=>{
+//         this.setState({clickCounter:this.state.clickCounter+1})
+//     }
+    
+    
+//         render(){
+//             return(
+//                 <>
+//                 <h2>{this.props.title}</h2>
+//                 <img src={this.props.imgUrl} />
+//                 <p>{this.props.description}</p>
+//                 </>    
+            
